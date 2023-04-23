fc6dbb51a83e6250ebba5ed38904f565a1fa2968
Joao Meyer <1994meyer@gmail.com>
2023-04-23 18:11:46 +0100

    Working

---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+<div class="bg-slate-950 h-screen flex justify-center text-slate-400">
+  <div class="flex flex-col justify-center max-w-[80%] lg:max-w-[40%]">
+    <slot></slot>
+  </div>
+</div>
+</template>
